e58583e22ee06cb00294bfeb324a12244d01a03f
Waqax19 <wa9p4eabrm@gmail.com>
2021-09-07 19:48:38 +0200

    Loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -8,5 +8,7 @@ public class ForLoop
         }
 
         System.out.println("i reacehd 11 and exit from the loop because the boolean expression is false atm");
+
+        System.out.println("You got that ???");
     }
 }
