12fbb7a5ee093b4845f048d6d917b5a5df124fc3
Waqax19 <wa9p4eabrm@gmail.com>
2021-09-11 18:56:40 +0200

    Loop

--- src/Collections/CompareY.java
+++ src/Collections/CompareY.java
@@ -12,6 +12,11 @@ public class CompareY implements Comparator<Point>
         double pointy1 = o1.getY();
         double pointy2 = o2.getY();
 
+        /*
+        ■ Define a class named CompareY that implements Comparator<Point>.
+        Implement the comparemethod to compare two points on their y-coordinates.
+        If two points have the same y-coordinates, compare their x-coordinates.
+         */
         if(pointy1 == pointy2)
         {
             if(pointx1 < pointx2)
---
+++ src/Collections/Stacks.java
@@ -0,0 +1,32 @@
+package Collections;
+
+import java.util.Stack;
+
+public class Stacks
+{
+    public static void main(String[] args)
+    {
+        Stack<Integer> myStack = new Stack<>();
+
+        boolean CheckEmpty = myStack.empty();
+
+        System.out.println("is stack empty ? " + CheckEmpty);
+
+
+        myStack.push(25);
+        myStack.push(87);
+        myStack.push(49);
+        myStack.push(27);
+        myStack.push(36);
+
+        System.out.println("Elemetns in stack : " + myStack);
+
+        CheckEmpty = myStack.empty();
+
+        System.out.println("If it is empty ? " + CheckEmpty);
+
+
+
+    }
+
+}
