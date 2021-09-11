01272895bd4edbf08d17f0154b93a3f83b2b13b3
Waqax19 <wa9p4eabrm@gmail.com>
2021-09-11 18:43:23 +0200

    Loop

--- src/Collections/SortingPoints.java
+++ src/Collections/SortingPoints.java
@@ -3,6 +3,18 @@ package Collections;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+(Sort points in a plane) Write a program that meets the following requirements:
+■ Define a class named Point with two data fields x and y to represent a
+point’s x- and y-coordinates. Implement the Comparable interface for comparing the points on x-coordinates. If two points have the same x-coordinates,
+compare their y-coordinates.
+■ Define a class named CompareY that implements Comparator<Point>.
+Implement the comparemethod to compare two points on their y-coordinates.
+If two points have the same y-coordinates, compare their x-coordinates.
+■ Randomly create 100 points and apply the Arrays.sort method to display
+the points in increasing order of their x-coordinates and in increasing order
+of their y-coordinates, respectively.
+ */
 public class SortingPoints
 {
     public static void main(String[] args)
