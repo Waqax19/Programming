8a2ae24cc8981a945ef2e1c78b175cd27f455a0f
Waqax19 <wa9p4eabrm@gmail.com>
2021-11-07 16:40:17 +0500

    BlockingQueue

---
+++ src/Threads/Producer.java
@@ -0,0 +1,37 @@
+package Threads;
+
+import javax.swing.tree.TreeCellEditor;
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable
+{
+    protected BlockingQueue queue = null;
+
+    public Producer(BlockingQueue queue)
+    {
+        this.queue = queue;
+    }
+
+
+    @Override
+    public void run()
+    {
+        try {
+            queue.put("1");
+
+            Thread.sleep(1000);
+
+            queue.put("2");
+
+            Thread.sleep(1000);
+
+            queue.put("3");
+
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
