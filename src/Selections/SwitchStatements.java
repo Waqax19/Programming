8babed3b44fd81b448eef95193f9c302738c41df
Waqax19 <wa9p4eabrm@gmail.com>
2021-12-27 15:19:21 +0200

    Switches

---
+++ src/Selections/SwitchStatements.java
@@ -0,0 +1,46 @@
+package Selections;
+
+import java.util.Scanner;
+
+public class SwitchStatements
+{
+    public static void main(String[] args)
+    {
+        int day = 45;
+
+        switch (day)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+
+            case 5:
+                System.out.println("Friday");
+                    break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("The value is not here OR incorrect input");
+        }
+    }
+}
