02cc7e461ff68fb4c012a886f8774a64b9c1c57c
Waqax19 <wa9p4eabrm@gmail.com>
2021-12-30 15:57:36 +0200

    String_comparing

--- src/Selections/ComparingString.java
+++ src/Selections/ComparingString.java
@@ -22,5 +22,43 @@ public class ComparingString
 
         System.out.println(name.compareTo(name5));//2
 
+        //contain function
+        String newName = "My name is Girogio ";
+
+        System.out.println(newName.contains("is"));
+
+        System.out.println(newName.contains("iro"));
+
+        System.out.println(newName.contains("Hello"));
+
+        System.out.println("--------------------------------\n");
+        String myString = "JohnAdams";
+
+        System.out.println("Original String : " + myString);
+
+        System.out.println("Substring starting from index 3 " + myString.substring(3));
+
+        System.out.println("Substring starting from index  0 to 3 " + myString.substring(0,3));
+
+        System.out.println("Welcomeo".indexOf('o'));
+
+        //Converting string to int
+
+        System.out.println("Convert string to int ");
+
+        String s = "500";
+
+        int i = Integer.parseInt(s);
+
+        System.out.println(s + 200);//concatenation, adding it into the string
+
+        System.out.println("1 + 3 =  " + (1 + 3));
+
+
+
+        System.out.println(name.concat(name3));
+
+        System.out.println(i + 200);//int
+
     }
 }
