0ae76e0e7c2484b57ecef0dd82c289635601e49e
Waqax19 <wa9p4eabrm@gmail.com>
2022-01-11 15:47:44 +0200

    ArrayIntro

---
+++ src/Arrayss/ArrDeclaration.java
@@ -0,0 +1,52 @@
+package Arrayss;
+
+public class ArrDeclaration
+{
+    public static void main(String[] args)
+    {
+        /*int arr[] = new int[5];//declaring and instatiation
+
+        arr[0] = 15;
+
+        arr[1] = 25;
+
+        arr[2] = 55;
+
+        arr[3] = 185;
+
+        arr[4] = 158;*/
+
+      //  int arr[] = {15,22,11,55,66,99,77,185};
+
+
+       /* int[] myArray = new int[50];
+
+        for (int i = 0; i < myArray.length; i++)
+        {
+            myArray[i] = (int)(Math.random() * 100);
+
+            System.out.println(myArray[i]);
+        }*/
+
+        char[] city = {'P','a','r','i','s'};
+
+        System.out.println(city);
+
+
+        //for-each loop
+
+        /*for (int i : arr)
+        {
+            System.out.println(i);
+        }*/
+
+
+       /* //traverse the array
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }*/
+
+
+    }
+}
